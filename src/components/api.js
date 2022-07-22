ca3cc1b7af36da9b9da0f4e77930142ee7b42fc4
Alexey Yanchev <yanchevalexey@yandex.ru>
2022-07-22 17:31:15 +0300

    done

--- src/components/api.js
+++ src/components/api.js
@@ -1,59 +1,138 @@
 import {checkPromiseResponse} from './utils.js';
 
 
-
-const config = {
-    baseUrl: 'https://mesto.nomoreparties.co/v1',
-    cohortId: 'plus-cohort-13',
-    headers: {
-      authorization: 'e4d16501-e8d2-438e-96b5-6b9c94c85c98',
-      'Content-Type': 'application/json'
+class Api {
+    constructor({config}) {
+        this._config = config;
+        this.data = null;
+        this._target = null;
+        this._headers = null;
+        this._card = null;
+        this._cardElement = null;
     }
-  }
 
-  const configTemplate = {
-    config: {
-        baseUrl: config.baseUrl, 
-        cohortId: config.cohortId,
-    }, 
-    options: {
-        headers: config.headers,
-}};
-  
-
-function requestPromiseFromURL(settings={
-    config: {
-        baseUrl: null, 
-        cohortId: null,
-     }, 
-     options: {}}, 
-    target='') {
-    if(settings.config.baseUrl && settings.config.cohortId && settings.options.headers.authorization) {
-        return (
-            fetch(`${settings.config.baseUrl}/${settings.config.cohortId}/${target}`, settings.options)
-        );
-    } else {
-        console.log('Не хватает свойств, переданных функции.');
+    _checkPromiseResponse(response) {
+        if(response.ok) {
+            return response.json();
+        } else {
+            return Promise.reject(`Ошибка: ${response.status} - ${response.statusText}`)
+        }
     }
-}
 
-function getDataOnRequestToServer(settings={configForRequest: {}, targetLink: ''}) {
-    return (
-        requestPromiseFromURL(settings.configForRequest, settings.targetLink)
-        .then(checkPromiseResponse)
+    _requestPromiseFromURL() {
+        if(this._config.baseUrl && this._config.cohortId && this._config.headers.authorization) {
+            return (
+                fetch(`${this._config.baseUrl}/${this._config.cohortId}/${this._target}`, {headers: this._config.headers})
+            );
+        } else {
+            console.log('Не хватает свойств, переданных функции.');
+        }
+    }
+
+    _deleteCardFromServer() {
+        this._headers.method = 'DELETE';
+        this.getDataOnRequestToServer({target: `cards/${this._cardObject._card.cardId}`})
         .then(data => {
-            return data;
+            console.log('Карточка удалена');
+            this._cardObject.removeCard();
+            this._cardObject._popupWithForm.closePopup();
         })
         .catch(error => {
-            console.log(error);
-            return false;
+            console.log('Карточка не удалена', error);
         })
-    );  
+        }
+
+    
+
+    confirmDeleteCallback(evt) {
+        evt.preventDefault();
+        this._deleteCardFromServer();
+    }
+
+    setCardObject({cardObject}) {
+        this._cardObject = cardObject;
+    }
+
+    getDataOnRequestToServer({target, headers}) {
+        this._config.headers = Object.assign(this._config.headers, headers);
+        this._target = target;
+        return (
+            this._requestPromiseFromURL()
+            .then(this._checkPromiseResponse)
+            .then(data => {
+                this.data = data;
+                return data;
+            })
+            .catch(error => {
+                console.log(error);
+                return false;
+            })
+        );  
+    }
+
+    // getDataOnRequestToServer({target, headers}) {
+    //     this._config.headers = Object.assign(this._config.headers, headers);
+    //     this._target = target;
+    //     this._requestPromiseFromURL()
+    //         .then(this._checkPromiseResponse)
+    //         .then(data => {
+    //             this.data = data;
+    //             console.log('logggg', this.data);
+    //             return data;
+    //         })
+    //         .catch(error => {
+    //             console.log(error);
+    //             return false;
+    //         });  
+    // return this.data;
 }
 
+    
+
+
+
+
+//   const configTemplate = {
+//     config: {
+//         baseUrl: config.baseUrl, 
+//         cohortId: config.cohortId,
+//     }, 
+//     options: {
+//         headers: config.headers,
+// }};
+  
+
+// function requestPromiseFromURL(settings={
+//     config: {
+//         baseUrl: null, 
+//         cohortId: null,
+//      }, 
+//      options: {}}, 
+//     target='') {
+//     if(settings.config.baseUrl && settings.config.cohortId && settings.options.headers.authorization) {
+//         return (
+//             fetch(`${settings.config.baseUrl}/${settings.config.cohortId}/${target}`, settings.options)
+//         );
+//     } else {
+//         console.log('Не хватает свойств, переданных функции.');
+//     }
+// }
+
+// function getDataOnRequestToServer(settings={configForRequest: {}, targetLink: ''}) {
+//     return (
+//         requestPromiseFromURL(settings.configForRequest, settings.targetLink)
+//         .then(checkPromiseResponse)
+//         .then(data => {
+//             return data;
+//         })
+//         .catch(error => {
+//             console.log(error);
+//             return false;
+//         })
+//     );  
+// }
+
 export {
-    config, 
-    configTemplate,
-    getDataOnRequestToServer,
+    Api,
 };
   
